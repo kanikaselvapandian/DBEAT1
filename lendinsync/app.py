9e8ac3fd760beb0e1172e59f10d80b4c867aa99c
weederek <weederek@gmail.com>
2023-10-26 14:53:11 +0800

    loan marketplace page

--- lendinsync/app.py
+++ lendinsync/app.py
@@ -5,6 +5,9 @@ app = Flask(__name__,template_folder="templates/")
 def hello():
   return render_template("login.html")
 
+@app.route("/loanmarketplace")
+def loanmarketplace():
+  return render_template("loanmarketplace.html")
 
 if __name__ == "__main__":
   app.run()
